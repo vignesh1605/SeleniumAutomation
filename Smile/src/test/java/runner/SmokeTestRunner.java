c4385d9c512978f918c8a850f4681818eabc4dc3
vignesh1605 <a.vignesh1605@outlook.com>
2026-03-19 10:51:18 +0530

    maven plugin

---
+++ Smile/src/test/java/runner/SmokeTestRunner.java
@@ -0,0 +1,21 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = {"src/test/resources/testdesign"},
+        glue = {"stepdefination"},
+        plugin = {"pretty",
+                "html:target/testreport/htmltrport.html",
+                "json:target/testreport/jsonreport.json",
+                "junit:target/testreport/xmlreport.xml"},
+        tags = "@smoketest"
+
+)
+
+public class SmokeTestRunner {
+}
